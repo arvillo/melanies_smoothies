3cc7df6cb247de6f93291af1962741f792c417d8
arvillo <arvin.melvillo@gmail.com>
2025-02-19 15:27:58 +0700

    Fix Search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ st.write('The Name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
 
 ingredient_list = st.multiselect(
     'Choose up to 5 ingridients:',
@@ -31,6 +32,10 @@ if ingredient_list:
     ingredients_string = ''
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st.subheader(fruit_chosen + ' Nutrition Information')
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
